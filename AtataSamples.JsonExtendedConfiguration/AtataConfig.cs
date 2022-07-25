b040cf63cef2cc5adc151db5c4589d9e344eee7d
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-25 22:29:18 +0200

    Add initial project files

---
+++ AtataSamples.JsonExtendedConfiguration/AtataConfig.cs
@@ -0,0 +1,11 @@
+﻿using Atata.Configuration.Json;
+
+namespace AtataSamples.JsonExtendedConfiguration
+{
+    public class AtataConfig : JsonConfig<AtataConfig>
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
